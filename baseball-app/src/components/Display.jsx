50f0591ac6f2a336e5a73602db203559b5388fc9
Alison Law <alisparklaw@gmail.com>
2020-02-13 12:36:31 +0000

    basic testing set up

--- baseball-app/src/components/Display.jsx
+++ baseball-app/src/components/Display.jsx
@@ -6,54 +6,46 @@ export default function Display() {
     // total display
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Total</h2>
+        <div className="orange">
+          <div className="totalandscore">
+            <h2 className="total">Total</h2>
 
-            <div className="home__score">{total}</div>
+            <div className="score">{total}</div>
           </div>
         </div>
       </section>
       <section className="buttons">
-        <div className="homeButtons">
-          <button
-            className="homeButtons__touchdown"
-            onClick={() => {
-              if (total <= 50) {
-                setTotal(total + 1);
-              } else {
-                setTotal(0);
-              }
-            }}
-          >
-           Increment by 1
-          </button>
-          <button
-            className="homeButtons__fieldGoal"
-            onClick={() => {
-              if (total <= 100000) {
-                setTotal(total + 10);
-              } else {
-                setTotal(0);
-              }
-            }}
-          >
-            Increment by 10
-          </button>
-          <button
-            className="homeButtons__fieldGoal"
-            onClick={() => setTotal(total+ 100)}
-          >
-            Increment by 100
-          </button>
-          <button
-            className="homeButtons__fieldGoal"
-            onClick={() => setTotal(0)}
-          >
-           Reset
-          </button>
-          <br />
-        </div>
+        <button
+          className="one"
+          onClick={() => {
+            if (total <= 50) {
+              setTotal(total + 1);
+            } else {
+              setTotal(0);
+            }
+          }}
+        >
+          Increment by 1
+        </button>
+        <button
+          className="ten"
+          onClick={() => {
+            if (total <= 100000) {
+              setTotal(total + 10);
+            } else {
+              setTotal(0);
+            }
+          }}
+        >
+          Increment by 10
+        </button>
+        <button className="ten" onClick={() => setTotal(total + 100)}>
+          Increment by 100
+        </button>
+        <button className="hundred" onClick={() => setTotal(0)}>
+          Reset
+        </button>
+        <br />
       </section>
     </div>
   );
--- baseball-app/src/components/Display.test.jsx
+++ baseball-app/src/components/Display.test.jsx
@@ -1,15 +1,35 @@
 import React from "react";
-import App from "../App";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Display from "./Display";
 
 afterEach(rtl.cleanup);
-describe;
-it("renders without crashing", () => {
-  // ready to test!
-  const wrapper = rtl.render(<Display />);
-});
+
+// test('sanity test', () => {
+//   const wrapper = rtl.render(<span className='scoreboard'>Total</span>)
+//   const scoreBoardHeader = wrapper.queryByText(/Total/i)
+//   expect(scoreBoardHeader).toBeInTheDocument()
+// })
+
+
+describe("Display",()=>{
+  it("renders without crashing", () => {
+    const wrapper = rtl.render(<Display />);
+        wrapper.debug()
+  });
+  
+  
+  // it('renders an button with a label "Increment by 1"', () => {
+
+  //   const wrapper = rtl.render(<Display />);
+  //   // 2- capture the element of interest
+  //   // const buttonOne= wrapper.getByRole("button") 
+   
+  //   // expect(buttonOne).toBeInTheDocument()
+  // })
+
+
+})
 
 // it("returns something ", () => {
 //   expect(setHomescore(homeScore).toBeDefined());
